c7d871fc93451f2f46884c3f8823791f8a94c2d4
SHAMIM MUNSHI <munshi6668@gmail.com>
2023-08-09 21:39:10 +0600

    Solved Class Work one and write the program without console project.

--- main.c
+++ main.c
@@ -1,21 +1,58 @@
+//==========================================Lab Task One================================================================
+/*
+1. Write a c programming code to print your ID, Name, Blood group, HSC  GPA. Use line breaks for every single element & include details using comment.
+2. Write a c programming code to get the below output-
+Bangladesh, 1971, dhaka
+*/
+/*
+//Two Problem Solution in One program
 #include <stdio.h>
-
 int main(){
-    //printf("Hello world!\n");
-    //Summation of Two Number using Function with return type int
-    //printf("The Summation of Two number 5 & 8 is = %d\n", summation(5,8));
-
-    //Multiplication using function
-    printf("The Multiplication of two number 8.5 & 5.5 is = %f", multiplication(8.500000000, 5.0554545454));
+    //Problem One -
+    printf("-----Problem One Solution-----\n\n");  //this print for output beautify
+    printf("ID : 2233091159\nName : SHAMIM MUNSHI\nBlood Group : O+\nDiploma CGPA - 3.56\n");  //Solution for Problem One
+    //Problem Two -
+    printf("\n\n-----Problem Two Solution-----\n\n");  //this print for output beautify
+    printf("Bangladesh, 1971, dhaka\n\nIndia, 1947 Delhi  #Kolkata\nPakistan, 1947+, \nIslamabad\n"); // solution for Problem Two
     return 0;
 }
+*/
+//Problem One Solution -
+/*
+#include <stdio.h>
+int main(){
+    //Problem Two Solution -
+    printf("ID : 2233091159\nName : SHAMIM MUNSHI\nBlood Group : O+\nDiploma CGPA - 3.56\n");  //Solution for Problem One
+    return 0;
+}
+*/
+//Problem Two Solution -
+/*
+#include <stdio.h>
+int main(){
+    //Problem Two Solution -
+    printf("Bangladesh, 1971, dhaka\n\nIndia, 1947 Delhi  #Kolkata\nPakistan, 1947+, \nIslamabad\n"); // solution for Problem Two
+    return 0;
+}
+*/
+//===========================================END LAB TASK ONE============================================================
 
-//Adding two number and return the summation
-int summation(int numberOne, int numberTwo){
-    return numberOne + numberTwo;
+#include <stdio.h>
+main(){
+    printf("Shamim Munshi");
 }
 
-int multiplication(double numberOne, double numberTwo){
-    return numberOne * numberTwo;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
