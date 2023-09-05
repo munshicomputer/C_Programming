cd583ffcc5a448831bd4ef2ad94aeabc3891b6c9
SHAMIM MUNSHI <munshi6668@gmail.com>
2023-09-05 22:11:08 +0600

    added some program

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,4 +2,9 @@
 
 main(){
     printf("Hello World");
+    printf("The Summation of Three Numbers : %d", summation(223311,60,48));
+}
+
+int summation(int numberOne, int numberTwo, int numberThree){
+    return numberOne + numberTwo + numberThree;
 }
